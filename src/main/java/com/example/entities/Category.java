ae01c033470bbeb4d0399088e33a102b4daf840c
Flynotfly <Alteria2004@gmail.com>
2025-01-22 17:47:23 +0400

    working get and post booking rest api

--- src/main/java/com/example/entities/Category.java
+++ src/main/java/com/example/entities/Category.java
@@ -9,14 +9,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "category")
 public class Category implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Column(nullable = false, unique = true)
